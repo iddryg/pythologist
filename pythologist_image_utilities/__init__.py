f3401ed0394bc8221d78aaad9b990c68bea41876
Jason Weirather <jason-weirather@users.noreply.github.com>
2022-04-21 14:34:30 -0400

    try to upgrade the binary dialation parameters to be closer

--- pythologist_image_utilities/__init__.py
+++ pythologist_image_utilities/__init__.py
@@ -52,7 +52,7 @@ def binary_image_dilation(np_array,steps=1,border_value=0,structure=[[0,1,0],[1,
         numpy.array: Image with that has been expanded
     """
     img = make_binary_image_array(np_array)
-    img = binary_dilation(img,iterations=steps,border_value=border_value,structure=np.array(structure),brute_force=True).astype(np.uint8)
+    img = binary_dilation(img,iterations=steps+1,border_value=border_value,structure=np.array(structure),brute_force=True).astype(np.uint8)
     return img
 
 def median_id_coordinates(np_array,exclude_points=None):
